07787832c8fe072638a865cc61e2bf27f2fdb6d7
Corey Donohoe <atmos@atmos.org>
2010-08-31 01:04:57 -0700

    showoff executable

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 // add the vendored express to the require path
-require.paths.unshift("vendor/ejs/lib")
-require.paths.unshift("vendor/showdown/lib")
-require.paths.unshift("vendor/connect/lib")
-require.paths.unshift("vendor/express/lib")
-require.paths.unshift("vendor/Socket.IO-node/lib")
+require("./lib/showoff")
 
 var app = require("./lib/app")
 
-app.run()
+app.run({presenting: false})
